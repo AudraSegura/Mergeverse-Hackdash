b14d4937fa3b2582f0498295164bf0f19e17556c
AudraSegura <alilyflower17@gmail.com>
2025-11-15 12:42:45 -0500

    Starting changes, not working

--- src/App.java
+++ src/App.java
@@ -1,20 +1,35 @@
+
 /* DEVELOPERS: Audra Segura, Sadia Firdous
  * DATE: 11-15-2025
  * DESCRIPTION: 
  */
+import java.io.File;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        double [] planetDistanceSun = {57.9, 108.2, 149.6, 227.9, 778.5, 1434, 2871, 4495};
-        int [] planetNumMoons = {0, 0, 1, 2, 79, 83, 27, 14};
-        String [] planetDescription = {
-            "The smallest and fastest planet—scorched on one side and frozen on the other due to its lack of atmosphere.",
-            "A hot, toxic world with thick clouds and extreme pressure—often called Earth’s “evil twin.”",
-            "The only known planet with life, rich blue oceans, and a protective atmosphere that supports diverse ecosystems.",
-            "A dusty red desert world with the largest volcano in the solar system and evidence of ancient water.",
-            "A massive gas giant with swirling storms, including the Great Red Spot, and dozens of diverse moons.",
-            "Known for its iconic icy rings and many moons, Saturn is a light, windy gas giant.",
-            "A cold, tilted ice giant that rotates sideways, giving it extreme seasons and a pale blue color.",
-            "The farthest major planet—a deep blue ice giant with strong winds and swirling storms."
+
+        File planets = new File("planets.txt");
+
+        if (!planets.exists()) {
+            System.out.println("File not found!");
+            return;
+        }
+        if (planets.length() == 0) {
+            System.out.println("File is empty!");
+            return;
+        }
+        Scanner sc = new Scanner(planets);
+
+        System.out.println("Working directory: " + System.getProperty("user.dir"));
+        System.out.println("testing print");
+        System.out.println(sc.hasNextLine());
+
+        while (sc.hasNextLine()) {
+            // System.out.println(sc.hasNextLine());
+            String test = sc.nextLine();
+            System.out.println("testing print in while loop");
+            System.out.println(test);
         }
 
     }
