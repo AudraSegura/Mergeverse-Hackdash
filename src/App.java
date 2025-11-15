6e0759f125d23d60d8b39d2ec8bd728ac693a6e6
AudraSegura <alilyflower17@gmail.com>
2025-11-15 11:55:22 -0500

    Distance, Moons, Description arrays

--- src/App.java
+++ src/App.java
@@ -4,6 +4,18 @@
  */
 public class App {
     public static void main(String[] args) throws Exception {
-        //dkfjghdifugy
+        double [] planetDistanceSun = {57.9, 108.2, 149.6, 227.9, 778.5, 1434, 2871, 4495};
+        int [] planetNumMoons = {0, 0, 1, 2, 79, 83, 27, 14};
+        String [] planetDescription = {
+            "The smallest and fastest planet—scorched on one side and frozen on the other due to its lack of atmosphere.",
+            "A hot, toxic world with thick clouds and extreme pressure—often called Earth’s “evil twin.”",
+            "The only known planet with life, rich blue oceans, and a protective atmosphere that supports diverse ecosystems.",
+            "A dusty red desert world with the largest volcano in the solar system and evidence of ancient water.",
+            "A massive gas giant with swirling storms, including the Great Red Spot, and dozens of diverse moons.",
+            "Known for its iconic icy rings and many moons, Saturn is a light, windy gas giant.",
+            "A cold, tilted ice giant that rotates sideways, giving it extreme seasons and a pale blue color.",
+            "The farthest major planet—a deep blue ice giant with strong winds and swirling storms."
+        }
+
     }
 }
